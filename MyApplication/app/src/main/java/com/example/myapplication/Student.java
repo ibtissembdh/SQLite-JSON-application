72c930bc17584f4f3e325be18553c6eca6f69cdf
ibtissembdh <ibtissem.bendouhou754@gmail.com>
2021-02-22 11:56:49 +0100

    student class

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Student.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class Student
+{
+    String name;
+    String familyName;
+    String ID;
+    public void Student(String name, String familyName, String ID)
+    {
+        this.name = name;
+        this.familyName = familyName;
+        this.ID =ID ;
+    }
+}
+
