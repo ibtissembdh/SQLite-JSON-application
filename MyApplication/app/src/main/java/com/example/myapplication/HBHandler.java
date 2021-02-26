db25de6c0f7d726ce919d9c13b031fbc8158511a
ibtissembdh <ibtissem.bendouhou754@gmail.com>
2021-02-26 20:20:43 +0100

    create student table in database

--- MyApplication/app/src/main/java/com/example/myapplication/HBHandler.java
+++ MyApplication/app/src/main/java/com/example/myapplication/HBHandler.java
@@ -1,4 +1,23 @@
 package com.example.myapplication;
 
-public class HBHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class HBHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION= 1;
+    private static final String DATABASE_NAME="dataBase";
+
+    public HBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE etudiant(_id INTEGER, matricule TEXT PRIMARY KEY, nom TEXT, prenom TEXT)"; db.execSQL(CREATE_TABLE);
+    }
+
+
+
+
 }
